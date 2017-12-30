51bf9af124f0df43008a2d9d5a4bbca357cbfc42
okusawa <okusawatakashi@gmail.com>
2017-12-30 23:20:03 +0900

    base64形式で画像を取得

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 /* -*- mode: web -*- */
 /* eslint no-console: 0 */
 /* eslint no-undef: 0 */
-/* eslint import/no-extraneous-dependencies: 0 */
-/* eslint global-require: 0 */
 
 import React from 'react';
+import Dimensions from 'Dimensions';
 import { StyleSheet, Image, Text, Button, View } from 'react-native';
+import Reactotron from 'reactotron-react-native';
 
 import { ImagePicker } from 'expo';
 
@@ -15,6 +15,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: Dimensions.get('window').width,
+    height: 400
+  }
 });
 
 if (__DEV__) {
@@ -24,26 +28,33 @@ if (__DEV__) {
 console.ignoredYellowBox = ['Setting a timer', 'source.uri'];
 
 type State = {
-  image: number
+  image: number,
+  base64: string
 };
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { image: null };
+    this.state = { image: null, base64: null };
     this.onPressPickImage = this.onPressPickImage.bind(this);
+    this.onPressOcr = this.onPressOcr.bind(this);
   }
 
   state: State;
 
+  onPressOcr() {
+    const { base64 } = this.state;
+  }
+
   onPressPickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
+      // allowsEditing: true,
+      // aspect: [4, 3],
+      base64: true
     });
-    // Reactotron.log(result);
+     Reactotron.log(result);
     if (!result.cancelled) {
-      this.setState({ image: result.uri });
+      this.setState({ image: result.uri, base64: result.base64 });
     }
   };
 
@@ -56,7 +67,12 @@ export default class App extends React.Component {
           onPress={this.onPressPickImage}
         />
         {image &&
-          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        <Image source={{ uri: image }} style={styles.image} />}
+        {image &&
+        <Button
+          title="execute ocr"
+          onPress={this.onPressOcr}
+        />}
       </View>
     );
   }
